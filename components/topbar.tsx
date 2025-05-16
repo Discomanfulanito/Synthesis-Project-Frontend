118c019ad170e34c694d26648310d82c1e9df756
discomanfulanito <pablonecpas@gmail.com>
2025-05-16 11:02:05 +0200

    relative margins

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -9,7 +9,7 @@ export default function Topbar ({isManager, setIsManager}: TopbarProps) {
 
 
     return (
-        <section className=" flex items-start gap-3 font-semibold mt-5 mb-auto ml-10 w-6xl">
+        <section  style={{marginLeft: "-35%"}} className=" bg-red flex items-start gap-3 font-semibold mt-5 mb-auto ml-10 w-6xl">
             <div className=" hover:cursor-pointer" onClick={()=>setIsManager(true)}>
                 Manage tasks
             </div>
--- components/translator-form.tsx
+++ components/translator-form.tsx
@@ -94,7 +94,7 @@ export default function TranslatorForm() {
     /*
     * Main
     */
-    <div className="container mx-auto px-4 py-8 max-w-6xl">
+    <div className="container my-auto mx-auto px-4 py-8 max-w-6xl">
       {/*
       * Title & Theme
       */}
